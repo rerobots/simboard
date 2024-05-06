bc97e1a2389aa61ceccc9e6016a8fa17f316b8e2
Scott C. Livingston <scott@rerobots.net>
2024-05-06 14:00:16 -0700

    more delay in example
    The real delay is less than I expect from the documentation
    of _delay_loop_2(). I might be using it incorrectly...

--- examples/avr/hola.c
+++ examples/avr/hola.c
@@ -11,9 +11,7 @@ https://www.nongnu.org/avr-libc/user-manual/group__avr__stdio.html
 #include <stdio.h>
 #include <avr/io.h>
 #include <avr/sleep.h>
-
-#define F_CPU (10000000UL)
-#include <util/delay.h>
+#include <util/delay_basic.h>
 
 
 static int uart_putchar(char c, FILE *stream)
@@ -31,7 +29,7 @@ static FILE ustdout = FDEV_SETUP_STREAM(uart_putchar, NULL, _FDEV_SETUP_WRITE);
 
 int main()
 {
-	uint8_t k;
+	uint8_t j, k;
 
 	stdout = &ustdout;
 
@@ -41,7 +39,8 @@ int main()
 
 	for (k = 0; k < 20; k++) {
 		PORTB = k;
-		_delay_ms(500);
+		for (j = 0; j < 100; j++)
+			_delay_loop_2(65535);
 	}
 
 	sleep_cpu();
